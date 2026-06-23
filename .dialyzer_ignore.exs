2e9692ea1781b5cf178395c53405a191bd04f97b
Jonathan Gao <gsmlg.com@gmail.com>
2026-06-23 23:24:59 +0800

    fix(ci): ignore fetch dialyzer false positive

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,6 +1,6 @@
 [
   # Dialyzer loses the Task.Supervisor.async_nolink/4 return shape in HTTP.fetch/2
-  ~r/lib\/http\.ex:259.*invalid_contract/,
+  ~r/lib\/http\.ex:259/,
   ~r/lib\/http\.ex:260.*no_return/,
 
   # HTTP.Promise.then/3 opaque type issue with Task struct - Task.Supervisor returns opaque Task
